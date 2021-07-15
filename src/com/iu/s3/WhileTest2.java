0b619d0e7ba999e083e7fae8c4cab0df637bd891
seongyeon-97 <creative9711@naver.com>
2021-07-15 10:18:10 +0900

    07/14 homework

--- src/com/iu/s3/WhileTest2.java
+++ src/com/iu/s3/WhileTest2.java
@@ -44,7 +44,7 @@ public class WhileTest2 {
 		//1->2 3마리 2->3 6마리 3->4 9마리
 		//레벌업 이모트 몬스터 1마리 사냥*3
 		//최종레벨, 최종골드 출력
-		int level=0;
+		int level=1;
 		int gold=0;
 		int hunt=0;
 		
@@ -52,7 +52,7 @@ public class WhileTest2 {
 			
 			System.out.println("스카니아 1서버 접속 완료");
 			
-			for(level=1; level<16; level++) {
+			for(level=1; level<15; level++) {
 				
 				System.out.println("="+level+"Level=");
 				
@@ -60,20 +60,22 @@ public class WhileTest2 {
 					System.out.println("몬스터"+hunt+"마리 사냥");
 				}
 				
-				if(level%5==0) {
-					gold=gold+1000;
-					System.out.println(gold+"GOLD 획득");
+				if(level%5==0) {					
+					gold=gold + (level/5)*1000;
+					System.out.println((level/5)*1000+"GOLD 획득");
 				}
 				
-				System.out.println("Level up");				
+				System.out.println("Level up");	
 				
-				if(level==15) {
+			}											
+				
 					System.out.println("만랩 달성하셨습니다!!!! 축하드립니다!!!");
 					System.out.println("최종 Level : "+level);
+					gold = gold + (level/5)*1000;
 					System.out.println("최종 Gold : "+gold);
 					System.out.println("=========Ending=========");
-				}
-			}
+				
+			
 		}
 		
 		System.out.println("----While Test2 finish----");
